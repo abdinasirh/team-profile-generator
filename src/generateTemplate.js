c3aa5e2ff2b6dabddf932c485cbee1825f1b7d85
Abdinasir Hassan <abdinasirduud20@gmail.com>
2021-12-22 09:48:57 -0600

    added some css styles

--- src/generateTemplate.js
+++ src/generateTemplate.js
@@ -97,13 +97,24 @@ module.exports = employees => {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Team Profile</title>
-        
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+        integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> 
+        <link rel="stylesheet" href="style.css">
+
+
     </head>
     <body>
         <header>
-            <h1>Team Profile</h1>
-        </header>
-    
+            <h2>Team Profile</h2>
+        </header>       
+        // <nav class="navbar navbar-light bg-light">
+        //     <span class="navbar-brand mb-0 h1">Team Profile</span>
+        // </nav>
+            
         <main>
             <div class="container">
                 ${generateTemplate(employees)}
